f114cf605210700fc90f6175eaf4a2261b375043
misocrash <Muskaan.Bahri@cognizant.com>
2025-09-09 16:54:51 +0530

    initial commit

---
+++ src/test/java/com/example/Meds/MedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Meds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
