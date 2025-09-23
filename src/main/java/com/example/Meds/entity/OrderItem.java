48711022f3b5d13893be6d269b47c5b4aaca9cdc
Sonkar <2438056@cognizant.com>
2025-09-23 12:22:55 +0530

    added order items

---
+++ src/main/java/com/example/Meds/entity/OrderItem.java
@@ -0,0 +1,67 @@
+package com.example.Meds.entity;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medicine_id", nullable = false)
+    private Medicine medicine;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(name = "price_at_purchase", nullable = false)
+    private BigDecimal priceAtPurchase;
+
+    public OrderItem(Long orderItemId, Order order, Medicine medicine, int quantity, BigDecimal priceAtPurchase) {
+        this.orderItemId = orderItemId;
+        this.order = order;
+        this.medicine = medicine;
+        this.quantity = quantity;
+        this.priceAtPurchase = priceAtPurchase;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Medicine getMedicine() {
+        return medicine;
+    }
+
+    public void setMedicine(Medicine medicine) {
+        this.medicine = medicine;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPriceAtPurchase() {
+        return priceAtPurchase;
+    }
+
+    public void setPriceAtPurchase(BigDecimal priceAtPurchase) {
+        this.priceAtPurchase = priceAtPurchase;
+    }
+}
